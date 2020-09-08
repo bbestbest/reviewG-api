9f46b48817319d5f3ee81f5cbbd39ff9c3f0a410
Naratip Supakong <naratipcover@gmail.com>
2020-09-08 19:00:08 +0700

    write routes

--- start/routes.js
+++ start/routes.js
@@ -19,3 +19,20 @@ const Route = use('Route')
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
+
+Route.group(() => { 
+
+  Route.resource('/accounts', 'AccountController')
+  
+  Route.resource('/admins', 'AdminController')
+
+  Route.resource('/user_scores', 'UserScoreController')
+
+  Route.resource('/admin_scores', 'AdminScoreController')
+
+  Route.resource('/comments', 'CommentController')
+
+  Route.resource('/posts', 'PostController')
+
+}) .prefix('api/reviewG')
+
