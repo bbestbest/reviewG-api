4ec85b45b3211ad620c11ff9e3e2bcc3f65ff8d6
Naratip Supakong <naratipcover@gmail.com>
2020-09-10 00:49:00 +0700

    add validator

---
+++ service/CommentValidator.js
@@ -0,0 +1,22 @@
+const Validator = use("Validator")
+
+module.exports = async function CommentValidator ( data ) {
+  if (typeof data !== 'object') throw new Error()
+
+  const { comment } = data
+
+  const rules = {
+    comment:'required'
+
+  }
+
+  const validation = await Validator.validateAll({
+    comment
+  }, rules)
+
+
+  return {
+    error: validation.messages()
+  }
+}
+
