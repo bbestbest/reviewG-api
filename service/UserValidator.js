8fc028c1075ef0667ecae6f190d6d04bf3875b5b
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-09-10 16:15:20 +0700

    change Account to User

---
+++ service/UserValidator.js
@@ -0,0 +1,22 @@
+const Validator = use("Validator")
+
+module.exports = async function AccountValidator ( data ) {
+  if (typeof data !== 'object') throw new Error()
+
+  const { username, email, password } = data
+
+  const rules = {
+    username: 'required',
+    email: 'required|email|unique:accounts,email',
+    password: 'required|min:8'
+  }
+
+  const validation = await Validator.validateAll({
+    username, email, password
+  }, rules)
+
+  return {
+    error: validation.messages()
+  }
+}
+
