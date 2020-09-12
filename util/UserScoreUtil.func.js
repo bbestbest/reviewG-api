8bd87689200b520f7d4cd4e2cbbd28e4808d35b7
Naratip Supakong <naratipcover@gmail.com>
2020-09-13 06:15:00 +0700

    add userscoreutil

---
+++ util/UserScoreUtil.func.js
@@ -0,0 +1,49 @@
+module.exports = function(UserScoreModel) {
+    const _withReferences = (references) => {
+        const _UserScore = UserScoreModel.query()
+            if(references) {
+                const extractedReferences = references.split(",")
+                extractedReferences.forEach((references) => _UserScore.with(references))
+            }
+        return _UserScore
+    }
+
+    return {
+        getAll: (references) => {
+            return _withReferences(references).fetch()
+        },
+        getByID: (user_score_id,references) => {
+            return _withReferences(references)
+                .where({user_score_id})
+                .fetch()
+                .then((response) => response.first())
+        },
+        create: async (attributes , references) => {
+            const {user_score_id} = await UserScoreModel.create(attributes)
+
+            return _withReferences(references)
+                .fetch()
+                .then((response) => response.first())
+        },
+        updateByID: async (user_score_id,attributes,references) => {
+            let userScore = await UserScoreModel.find(user_score_id)
+            userScore.merge(attributes)
+            await userScore.save()
+
+            return _withReferences(references)
+                .where({user_score_id})
+                .fetch()
+                .then((response) => response.first())
+
+        },
+        deleteByID: async (user_score_id) => {
+            const userScore = await UserScoreModel.find(user_score_id)
+            if(userScore !== null) {
+                return userScore.delete()
+            }
+            else {
+                return userScore
+            }
+        }
+    }
+}
