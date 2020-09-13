e2d827fa60582b80ced4c026e8d27a7df7661711
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-09-13 19:50:21 +0700

    add OverallScore

--- test/unit/OverallScore.spec.js
+++ test/unit/OverallScore.spec.js
@@ -1,19 +1,19 @@
-// 'use strict'
+'use strict'
 
-// const { test } = use('Test/Suite')('OverAllScore')
-// const overallScore = require('../../util/OverAllScoreUtil')
+const { test } = use('Test/Suite')('OverAllScore')
+const overallScore = require('../../util/OverAllScoreUtil')
 
-// test('Overall score should return float number', async ({ assert }) => {
-//   const story = 1
-//   const gameplay = 2
-//   const performance = 3
-//   const graphic = 4
-//   const target = (story+gameplay+performance+graphic)/4
-//   const overall = await overallScore(story,gameplay,performance,graphic)
-//   assert.equal(target,overall)
-// }),
-// test('Overall score should return a', async ({ client }) => {
-//   const user = {
+test('Overall score should return float number', async ({ assert }) => {
+  const story = 1
+  const gameplay = 2
+  const performance = 3
+  const graphic = 4
+  const target = (story+gameplay+performance+graphic)/4
+  const overall = await overallScore(story,gameplay,performance,graphic)
+  assert.equal(target,overall)
+}),
+test('Overall score should return a', async ({ client }) => {
+  const user = {
 
-//   }
-// })
+  }
+})
