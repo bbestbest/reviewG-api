b0bb38c7e10241a5e710d0006ad5e4f25e108f1a
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-09-14 15:06:06 +0700

    remove unsed test

--- test/unit/OverallScore.spec.js
+++ test/unit/OverallScore.spec.js
@@ -3,7 +3,7 @@
 const { test } = use('Test/Suite')('OverAllScore')
 const overallScore = require('../../util/OverAllScoreUtil')
 
-test('Overall score should return float number', async ({ assert }) => {
+test('Overall score should return target float number', async ({ assert }) => {
   const story = 1
   const gameplay = 2
   const performance = 3
@@ -11,9 +11,4 @@ test('Overall score should return float number', async ({ assert }) => {
   const target = (story+gameplay+performance+graphic)/4
   const overall = await overallScore(story,gameplay,performance,graphic)
   assert.equal(target,overall)
-}),
-test('Overall score should return a', async ({ client }) => {
-  const user = {
-
-  }
 })
