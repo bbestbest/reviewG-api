f948eebcf347c52fdb85d0c1b4a19e07940086e3
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-10-27 01:10:56 +0700

    add must store admin_id

--- app/Controllers/Http/AdminScoreController.js
+++ app/Controllers/Http/AdminScoreController.js
@@ -20,7 +20,7 @@ class AdminScoreController {
       const { references } = request.qs
       const validateValue = numberTypeParamValidator(id)
 
-      if (validateValue.error) 
+      if (validateValue.error)
         return {status: 500, error: validateValue.error, data: undefined }
 
       const adminScore = await AdminScoreUtil(AdminScoreModel).getByID(id,references)
@@ -29,24 +29,24 @@ class AdminScoreController {
     }
 
     async store({request}) {
-      const { story,gameplay,performance,graphic } = request.body
+      const { story,gameplay,performance,graphic,admin_id } = request.body
       const { references } = request.qs
 
       const validatedData = await AdminScoreValidator(request.body)
-        
+
       if (validatedData.error)
         return { status: 422, error: validatedData.error, data: undefined }
 
       const overall = OverAllScore(parseFloat(story),parseFloat(gameplay),parseFloat(performance),parseFloat(graphic))
 
-      const adminScore = await AdminScoreUtil(AdminScoreModel).create({story,gameplay,performance,graphic,overall},references)
-    
+      const adminScore = await AdminScoreUtil(AdminScoreModel).create({story,gameplay,performance,graphic,overall,admin_id},references)
+
       return { status: 200, error: undefined, data: adminScore }
 
     }
 
     async update({request}){
-  
+
       const { body,params,qs } = request
       const { id } = params
       const { story,gameplay,performance,graphic} = body
@@ -55,17 +55,17 @@ class AdminScoreController {
       const overall = OverAllScore(parseFloat(story),parseFloat(gameplay),parseFloat(performance),parseFloat(graphic))
 
       const adminScore = await AdminScoreUtil(AdminScoreModel).updateByID(id,{story,gameplay,performance,graphic,overall},references)
-  
+
       return {status: 200 , error: undefined, data: adminScore}
       }
-  
+
   async destroy ({ request }) {
     const { params , qs } =request
     const { id } = params
     const { references} = qs
 
     const adminScore = await AdminScoreUtil(AdminScoreModel).deleteByID(id)
-    
+
     if(adminScore) {
         return {status: 200 , error: undefined, data: { massage: ' success' }}
     }
